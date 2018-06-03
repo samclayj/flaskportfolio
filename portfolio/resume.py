f9e0c7f18a30b6f49042f03443f0ff5128d6688b
Sam Jentsch <samj@Sams-MacBook-Pro.local>
2018-06-03 14:14:07 -0700

    Rendered charts using templating

--- portfolio/resume.py
+++ portfolio/resume.py
@@ -3,9 +3,17 @@ from flask import (
 )
 from werkzeug.exceptions import abort
 
+import json
+
 bp = Blueprint('resume', __name__)
 
 
 @bp.route('/')
 def index():
     return render_template('resume/resume.html')
+
+@bp.route('/charts')
+def charts():
+    labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July']
+    json_data = json.dumps([1,2,3,{'4': 5, '6': 7}], separators=(',',':'))
+    return render_template('charts/test-chart.html', data=json_data)
