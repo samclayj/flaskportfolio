53b0f4bb5bb80eac6833c162ef0af5d18dc212ef
Sam Jentsch <samj@Sams-MacBook-Pro.local>
2018-06-08 07:40:42 -0700

    Resume almost done

--- portfolio/resume.py
+++ portfolio/resume.py
@@ -6,8 +6,6 @@ from werkzeug.exceptions import abort
 import json
 import sys
 
-from charts import Chart, ChartTypes
-
 bp = Blueprint('resume', __name__)
 
 
@@ -17,34 +15,4 @@ def index():
 
 @bp.route('/charts')
 def charts():
-    labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July']
-    data = [0, 10, 5, 2, 20, 30, 45]
-    data2 = [0, 20, 1, 8, 30, 50, 25]
-
-    bubbleData = [{'x': 1, 'y': 10, 'r': 50}, {'x': 2, 'y': 10, 'r': 30}]
-
-    chart = Chart(ChartTypes.LINE)
-    chart.set_labels(labels)
-    chart.create_and_add_dataset(
-            label='First dataset',
-            bgColor='blue',
-            borderColor='green',
-            data=data)
-
-    chart.create_and_add_dataset(
-            label='Second dataset',
-            bgColor='rgb(255, 99, 132)',
-            borderColor='rgb(255, 99, 132)',
-            data=data2)
-
-
-    bubbleChart = Chart(ChartTypes.BUBBLE)
-    bubbleChart.create_and_add_dataset(
-            label='Bubble dataset',
-            bgColor='blue',
-            borderColor='green',
-            data=bubbleData)
-
-    return render_template('charts/test-chart.html',
-            chart=chart.get_nonescaped_json(),
-            bubbleChart=bubbleChart.get_nonescaped_json())
+    return render_template('charts/test-chart.html')
--- portfolio/static/js/scrollin.js
+++ portfolio/static/js/scrollin.js
@@ -1,15 +1,18 @@
-$(document).on("scroll", function () {
-    let pageTop = $(document).scrollTop();
-    let pageBottom = pageTop + $(window).height();
-    let tags = $(".scroll-in");
+const ScrollIn = (function() {
 
-    for (let i = 0; i < tags.length; i++) {
-        let tag = tags[i];
+    let scrollInBehavior = function(pageTop, pageBottom) {
+        const tags = $(".scroll-in");
 
-        if ($(tag).position().top < pageBottom) {
-            $(tag).addClass("visible");
-        } else {
-            $(tag).removeClass("visible");
+        for (const tag of tags) {
+            if ($(tag).position().top < pageBottom) {
+                $(tag).addClass("visible");
+            } else {
+                $(tag).removeClass("visible");
+            }
         }
     }
-});
+
+    return {
+        scrollInBehavior: scrollInBehavior,
+    }
+})();
